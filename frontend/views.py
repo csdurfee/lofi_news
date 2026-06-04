f5c8a47e41e282dc9e0e02b5aa03eb62e15df0fd
casey <csdurfee@gmail.com>
2026-06-04 07:27:20 -1000

    basic limit/offset

--- frontend/views.py
+++ frontend/views.py
@@ -6,5 +6,13 @@ logger = logging.getLogger(__name__)
 
 def index(request):
     #logger.error("hoo boy")
-    stories = Story.objects.select_related('data_source').order_by('-retrieved')
-    return render(request, 'frontend/index.html', {'stories': stories})
+    # TODO: validation
+    # TODO: these should be coming from datastar signal, probably
+    limit = request.GET.get('limit', 10)
+    offset = request.GET.get('offset', 0)
+
+    stories = Story.objects \
+                .select_related('data_source') \
+                .order_by('-retrieved')[offset : offset+limit]
+    return render(request, 'frontend/index.html', 
+                  {'stories': stories})
