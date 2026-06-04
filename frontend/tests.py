99e00124175a2bc4a28146aa30418e8aaf05b9b5
casey <csdurfee@gmail.com>
2026-06-04 07:26:56 -1000

    basic smoke test

--- frontend/tests.py
+++ frontend/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexViewTest(TestCase):
+    def test_index_returns_200_with_content(self):
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertGreater(len(response.content), 0)
