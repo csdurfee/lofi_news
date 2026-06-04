d071bfeed1bb8d6e13dbcd3068960d2f1b4379e3
casey <csdurfee@gmail.com>
2026-06-04 12:13:34 -1000

    fix logging config boondoggle

--- backend/scrape.py
+++ backend/scrape.py
@@ -5,32 +5,41 @@ import sys
 
 import strategy.rss
 
+# this is necessary so we can use frontend.models in another package
+# (I think)
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lofi_news.settings')
 django.setup()
 
 from frontend import models
 
-# this is a little funky because it's not using django's default log
-# config. but simplicity wins here.
+# FIXME: there is probably a better way to do this...
+# django's logging config will cause log events to go to the default
+# django log, which we don't want. so we have to remove the existing
+# log handlers.
+# https://stackoverflow.com/questions/12158048/changing-loggings-basicconfig-which-is-already-set
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
+
 LOG_DIR = os.path.join(os.path.dirname(__file__), 'logs')
+LOGFILE = os.path.join(LOG_DIR, 'scrape.log')
+
 logging.basicConfig(
-    filename=os.path.join(LOG_DIR, 'scrape.log'),
-    level=logging.INFO,
-    format='%(asctime)s %(levelname)s %(message)s',
-)
+    filename=LOGFILE,
+    level=logging.DEBUG,
+    format="{levelname} {asctime} {module} {message}",
+    style="{"
+    )
 logger = logging.getLogger(__name__)
 
-if __name__ == '__main__':
+def main():
     sources = models.DataSource.objects.all()
-    logger.info("Found {len(sources)} data sources")
+    logger.info(f"Found {len(sources)} data sources")
     for source in sources:
         logger.info(f"Fetching {source}")
         if source.type == 'rss':
             stories = strategy.rss.scrape(source.url)
-            #print(scraped)
             for story in stories:
-                # TODO: check for uniqueness based on `original_url`
                 # uniqueness is tested based on original URL.
                 if models.Story.objects.filter(original_url=story['original_url']).exists():
                     logger.info(f"story {story['original_url']} already exists")
@@ -41,6 +50,8 @@ if __name__ == '__main__':
                                             raw_data = story['raw_data'],
                                             text = story['text'])
                     story_model.save()
-                    logger.info("created story %s", story_model)
+                    logger.info(f"created story {story_model}")
 
+if __name__ == '__main__':
+    main()
     
