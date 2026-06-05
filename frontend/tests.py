0e089a65524767d8d4e3c59fb1d7047ecd5c2e94
casey <csdurfee@gmail.com>
2026-06-05 09:58:04 -1000

    ensure no GET requests to vote endpoints

--- frontend/tests.py
+++ frontend/tests.py
@@ -7,3 +7,9 @@ class IndexViewTest(TestCase):
         response = self.client.get(reverse("index"))
         self.assertEqual(response.status_code, 200)
         self.assertGreater(len(response.content), 0)
+
+
+class VoteViewTest(TestCase):
+    def test_get_vote_returns_405(self):
+        response = self.client.get("/vote/up/276")
+        self.assertEqual(response.status_code, 405)
