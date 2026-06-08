41712de3ea41a14e56bcd2638f71ac86528dff28
casey <csdurfee@gmail.com>
2026-06-08 07:29:39 -1000

    selecting by source (this is not the best way to do it with datastar)

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,9 +4,14 @@ from .views import index, join, user_settings, vote
 
 urlpatterns = [
      path("",  index.index, name="index"),
+     
+     path("more/<source_code>", index.more, name="source_more"),
      path("more", index.more, name="more"),
+
+     path("about", index.about, name="about"),
+     path("source/<source_code>", index.index, name="source"),
+
      path("join", join.join, name="join"),
      path("settings", user_settings.user_settings, name="settings"),
-     path("about", index.about, name="about"),
      path("vote/<direction>/<int:story_id>", vote.vote),
 ]
--- frontend/views/index.py
+++ frontend/views/index.py
@@ -1,4 +1,4 @@
-from frontend.models import Story, Vote
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.views.decorators.http import require_http_methods
@@ -7,16 +7,38 @@ from datastar_py.django import (DatastarResponse, read_signals)
 from datastar_py.django import ServerSentEventGenerator as SSE
 from datastar_py.consts import ElementPatchMode
 
+from frontend.models import Story, Vote, DataSource
+
 import logging
 logger = logging.getLogger(__name__)
 
-def index(request):
-    limit = request.GET.get('limit', 10)
-    if limit > 100:
-        limit = 100
+ALL_SOURCES = 1
 
-    stories = list(Story.objects.order_by("-id") \
-                .select_related('data_source')[:limit])
+def _get_stories(order_by="-id", limit=10, sources=ALL_SOURCES, last_id=None):
+    # TODO: refactor this so I can dynamically add filters, so it works with /more
+    query = Story.objects.order_by(order_by)
+    if sources != ALL_SOURCES:
+        query = query.filter(data_source__in=sources)
+    if last_id:
+        query = query.filter(id__lt=last_id)
+    
+    stories = list(query.select_related('data_source')[:limit])
+
+    return stories
+
+
+def index(request, source_code=None):
+    limit = 10
+
+    if source_code == None:
+        stories = _get_stories(limit=limit)
+    else:
+        # get ID for source
+        sources = DataSource.objects.filter(code=source_code)
+        if len(sources) == 0:
+            return HttpResponseNotFound()
+        else:
+            stories = _get_stories(limit=limit, sources=sources)
 
     last_id = stories[limit-1].id
     story_ids = {story.id for story in stories}
@@ -28,8 +50,6 @@ def index(request):
 
     # get user settings. TODO: factor out into own UserSettings component.
     new_tabs = request.session.get('new_tabs', 0)
-
-    logger.debug(f"new_tabs is {new_tabs}")
 
     return render(request, 'frontend/index.html', 
                   {'stories': stories,
@@ -51,15 +71,15 @@ def no_stories():
     )
 
 @require_http_methods(['GET'])
-def more(request):
+def more(request, sources=ALL_SOURCES):
     signals = read_signals(request)
     logger.info("got signals %r" % signals)
 
     if signals and ('lastId' in signals):
-        lastId = signals['lastId']
+        last_id = signals['lastId']
         limit = 10
 
-        stories = list(Story.objects.order_by("-id").filter(id__lt=lastId)[:limit])
+        stories = _get_stories(sources=sources, limit=limit, last_id=last_id)
 
         if len(stories) == 0:
             return no_stories()
@@ -73,18 +93,19 @@ def more(request):
                         {'stories': stories,
                          'votes_on_page': votes_on_page,
                          }, request=request)
-            newLastId = stories[limit-1].id
+            new_last_id = stories[limit-1].id
 
             # we can just return an array to DatastarResponse.
             return DatastarResponse(
                 [
+                # default mode replaces the selector; we want to add to end
                 SSE.patch_elements(rendered,
                                     selector="#stories",
                                     mode=ElementPatchMode.APPEND
                 ),
                 # note: it's NOT kebab case for signals sent from server
                 SSE.patch_signals(
-                            {"lastId": newLastId }
+                            {"lastId": new_last_id }
                 ),
                 ]
             )
